cebe8e5f83570f3c7ef7bc94a57efa0959d98db7
Danny Raymond Pugh <drpugh@stanford.edu>
2021-10-28 14:19:25 -0700

    Add and tested problem 3ii

---
+++ Problem_2/Test.py
@@ -0,0 +1,7 @@
+# import numpy as np
+# from numpy.lib.function_base import append
+# from PlotFunctions import *
+# from ExtractLines import FitLine
+# import matplotlib.pyplot as plt
+
+# theta = np.array([])
